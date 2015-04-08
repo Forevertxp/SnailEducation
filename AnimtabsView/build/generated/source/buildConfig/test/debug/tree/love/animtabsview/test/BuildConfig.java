a50c2939708ff224295e220dfc387fc806f38d9b
Forevertxp <forevertxp@gmail.com>
2015-04-08 17:52:01 +0800

    第三方tab

---
+++ AnimtabsView/build/generated/source/buildConfig/test/debug/tree/love/animtabsview/test/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package tree.love.animtabsview.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "tree.love.animtabsview.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+  /**
+   * @deprecated Use {@link #APPLICATION_ID}
+   */
+  @Deprecated
+  public static final String PACKAGE_NAME = "tree.love.animtabsview.test";
+}
